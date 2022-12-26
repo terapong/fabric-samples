4f06440467e772b26f6adad7f666789066f4cd35
terapong <terapong.p@gmail.com>
2022-12-26 09:48:45 +0700

    salabun _1_ok

--- salabun/go/salabun.go
+++ salabun/go/salabun.go
@@ -14,6 +14,23 @@ type SmartContract struct {
 }
 
 // Asset describes basic details of what makes up a simple asset
+
+/*สารบรรณ ลงทะเบียนหนังสือรับ
+ประเภทหนังสือ     ptsw_book_type           --ภายนอง ถายใน
+เลขทะเบียนรับ     ptsw_book_receive_number --text
+รับวันที่          ptsw_book_receive_date    --cal         
+ชั้นความลับ       ptsw_book_secret_class   --ปกติ           ///ด่วน ด่วนมาก ด่วนที่สุด 
+เลขที่หนังสือ       ptsw_book_number         --text   
+ลงวันที่           ptsw_book_go_date       --cal
+จาก             ptsw_book_from          --text
+ถึง              ptsw_book_to            --text
+เรื่อง            ptsw_book_subject       --text
+การปฎิบัติ         ptsw_book_mission       --text          ///ต้องเป็น ให้ใครไปทำ หน่วยงานที่ทำ ต้องทำอีก
+หมาบเหตุ         ptsw_book_note          --text
+แฟ้มเก็บ          ptsw_book_fame          --text
+อัปโหลดไฟล์       ptsw_book_file          --dowload file  ///หลายไฟล์
+บันทึกข้อมูล
+
 type Asset struct {
   ID             string `json:"ID"`
   Color          string `json:"color"`
@@ -25,16 +42,104 @@ type Asset struct {
   BookType       int    `json:"bookType"`
 }
 
+*/
+
+type Asset struct {
+  ID                        string `json:"ID"`
+  Ptsw_book_type            string `json:"ptsw_book_type"`
+  Ptsw_book_receive_number  string `json:"ptsw_book_receive_number"`
+  Ptsw_book_receive_date    string `json:"ptsw_book_receive_date"`
+  Ptsw_book_secret_class    string `json:"ptsw_book_secret_class"`
+  Ptsw_book_number          string `json:"ptsw_book_number"`
+  Ptsw_book_go_date         string `json:"ptsw_book_go_date"`
+  Ptsw_book_from            string `json:"ptsw_book_from"`
+  Ptsw_book_to              string `json:"ptsw_book_to"`
+  Ptsw_book_subject         string `json:"ptsw_book_subject"`
+  Ptsw_book_mission         string `json:"ptsw_book_mission"`
+  Ptsw_book_note            string `json:"ptsw_book_note"`
+  Ptsw_book_fame            string `json:"ptsw_book_fame"`
+  Ptsw_book_file            string `json:"ptsw_book_file"`
+}
+
 // InitLedger adds a base set of assets to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
 
   assets := []Asset{
-    {ID: "asset1", Color: "blue", Size: 5, Owner: "Tomoko", AppraisedValue: 300, From: "Terapong", Too: "potisuwan", BookType: 1} , 
-    {ID: "asset2", Color: "red", Size: 5, Owner: "Brad", AppraisedValue: 400, From: "Terapong", Too: "potisuwan", BookType: 2},
-    {ID: "asset3", Color: "green", Size: 10, Owner: "Jin Soo", AppraisedValue: 500, From: "Terapong", Too: "potisuwan", BookType: 3},
-    {ID: "asset4", Color: "yellow", Size: 10, Owner: "Max", AppraisedValue: 600, From: "Terapong", Too: "potisuwan", BookType: 2},
-    {ID: "asset5", Color: "black", Size: 15, Owner: "Adriana", AppraisedValue: 700, From: "Terapong", Too: "potisuwan", BookType: 1},
-    {ID: "asset6", Color: "white", Size: 15, Owner: "Michel", AppraisedValue: 800, From: "Terapong", Too: "potisuwan", BookType: 3},
+    {ID: "1", 
+      Ptsw_book_type:           "Ptsw_book_type_1", 
+      Ptsw_book_receive_number: "Ptsw_book_receive_number_1",
+      Ptsw_book_receive_date:   "Ptsw_book_receive_date_1",
+      Ptsw_book_secret_class:   "Ptsw_book_secret_class_1",
+      Ptsw_book_number:         "Ptsw_book_number_1",
+      Ptsw_book_go_date:        "Ptsw_book_go_date_1",
+      Ptsw_book_from:           "Ptsw_book_from_1",
+      Ptsw_book_to:             "Ptsw_book_to_1",
+      Ptsw_book_subject:        "Ptsw_book_subject_1",
+      Ptsw_book_mission:        "Ptsw_book_mission_1",
+      Ptsw_book_note:           "Ptsw_book_note_1",
+      Ptsw_book_fame:           "Ptsw_book_fame_1",
+      Ptsw_book_file:           "Ptsw_book_file_1",
+      },
+      {ID: "2", 
+      Ptsw_book_type:           "Ptsw_book_type_2", 
+      Ptsw_book_receive_number: "Ptsw_book_receive_number_2",
+      Ptsw_book_receive_date:   "Ptsw_book_receive_date_2",
+      Ptsw_book_secret_class:   "Ptsw_book_secret_class_2",
+      Ptsw_book_number:         "Ptsw_book_number_2",
+      Ptsw_book_go_date:        "Ptsw_book_go_date_2",
+      Ptsw_book_from:           "Ptsw_book_from_2",
+      Ptsw_book_to:             "Ptsw_book_to_2",
+      Ptsw_book_subject:        "Ptsw_book_subject_2",
+      Ptsw_book_mission:        "Ptsw_book_mission_2",
+      Ptsw_book_note:           "Ptsw_book_note_2",
+      Ptsw_book_fame:           "Ptsw_book_fame_2",
+      Ptsw_book_file:           "Ptsw_book_file_2",
+      },
+      {ID: "3", 
+      Ptsw_book_type:           "Ptsw_book_type_3", 
+      Ptsw_book_receive_number: "Ptsw_book_receive_number_3",
+      Ptsw_book_receive_date:   "Ptsw_book_receive_date_3",
+      Ptsw_book_secret_class:   "Ptsw_book_secret_class_3",
+      Ptsw_book_number:         "Ptsw_book_number_3",
+      Ptsw_book_go_date:        "Ptsw_book_go_date_3",
+      Ptsw_book_from:           "Ptsw_book_from_3",
+      Ptsw_book_to:             "Ptsw_book_to_3",
+      Ptsw_book_subject:        "Ptsw_book_subject_3",
+      Ptsw_book_mission:        "Ptsw_book_mission_3",
+      Ptsw_book_note:           "Ptsw_book_note_3",
+      Ptsw_book_fame:           "Ptsw_book_fame_3",
+      Ptsw_book_file:           "Ptsw_book_file_3",
+      },
+      {ID: "4", 
+      Ptsw_book_type:           "Ptsw_book_type_4", 
+      Ptsw_book_receive_number: "Ptsw_book_receive_number_4",
+      Ptsw_book_receive_date:   "Ptsw_book_receive_date_4",
+      Ptsw_book_secret_class:   "Ptsw_book_secret_class_4",
+      Ptsw_book_number:         "Ptsw_book_number_4",
+      Ptsw_book_go_date:        "Ptsw_book_go_date_4",
+      Ptsw_book_from:           "Ptsw_book_from_4",
+      Ptsw_book_to:             "Ptsw_book_to_4",
+      Ptsw_book_subject:        "Ptsw_book_subject_4",
+      Ptsw_book_mission:        "Ptsw_book_mission_4",
+      Ptsw_book_note:           "Ptsw_book_note_4",
+      Ptsw_book_fame:           "Ptsw_book_fame_4",
+      Ptsw_book_file:           "Ptsw_book_file_4",
+      },
+      {ID: "5", 
+      Ptsw_book_type:           "Ptsw_book_type_5", 
+      Ptsw_book_receive_number: "Ptsw_book_receive_number_5",
+      Ptsw_book_receive_date:   "Ptsw_book_receive_date_5",
+      Ptsw_book_secret_class:   "Ptsw_book_secret_class_5",
+      Ptsw_book_number:         "Ptsw_book_number_5",
+      Ptsw_book_go_date:        "Ptsw_book_go_date52",
+      Ptsw_book_from:           "Ptsw_book_from_5",
+      Ptsw_book_to:             "Ptsw_book_to_5",
+      Ptsw_book_subject:        "Ptsw_book_subject_5",
+      Ptsw_book_mission:        "Ptsw_book_mission_5",
+      Ptsw_book_note:           "Ptsw_book_note_5",
+      Ptsw_book_fame:           "Ptsw_book_fame_5",
+      Ptsw_book_file:           "Ptsw_book_file_5",
+      },
   }
 
   for _, asset := range assets {
@@ -53,8 +158,22 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 }
 
 // CreateAsset issues a new asset to the world state with given details.
-  func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, color string, 
-    size int, owner string, appraisedValue int, from string, too string, bookType int) error {
+  func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, 
+    id                        string, 
+    ptsw_book_type            string, 
+    ptsw_book_receive_number  string,
+    ptsw_book_receive_date    string,
+    ptsw_book_secret_class    string,
+    ptsw_book_number          string,
+    ptsw_book_go_date         string,
+    ptsw_book_from            string,
+    ptsw_book_to              string,
+    ptsw_book_subject         string,
+    ptsw_book_mission         string,
+    ptsw_book_note            string,
+    ptsw_book_fame            string,
+    ptsw_book_file            string, 
+    ) error {
   exists, err := s.AssetExists(ctx, id)
   if err != nil {
     return err
@@ -64,15 +183,20 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
   }
 
   asset := Asset{
-    ID:             id,
-    Color:          color,
-    Size:           size,
-    Owner:          owner,
-    AppraisedValue: appraisedValue,
-    From: from,
-    Too: too,
-    BookType: bookType,
-    
+    ID:                       id,
+    Ptsw_book_type:           ptsw_book_type,
+    Ptsw_book_receive_number: ptsw_book_receive_number,
+    Ptsw_book_receive_date:   ptsw_book_receive_date,
+    Ptsw_book_secret_class:   ptsw_book_secret_class,
+    Ptsw_book_number:         ptsw_book_number,
+    Ptsw_book_go_date:        ptsw_book_go_date,
+    Ptsw_book_from:           ptsw_book_from,
+    Ptsw_book_to:             ptsw_book_to,
+    Ptsw_book_subject:        ptsw_book_subject,
+    Ptsw_book_mission:        ptsw_book_mission,
+    Ptsw_book_note:           ptsw_book_note,
+    Ptsw_book_fame:           ptsw_book_fame,
+    Ptsw_book_file:           ptsw_book_file, 
   }
   assetJSON, err := json.Marshal(asset)
   if err != nil {
@@ -102,8 +226,22 @@ func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, i
 }
 
 // UpdateAsset updates an existing asset in the world state with provided parameters.
-  func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, id string, color string, 
-    size int, owner string, appraisedValue int, from string, too string, bookType int) error {
+  func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, 
+    id                        string, 
+    ptsw_book_type            string, 
+    ptsw_book_receive_number  string,
+    ptsw_book_receive_date    string,
+    ptsw_book_secret_class    string,
+    ptsw_book_number          string,
+    ptsw_book_go_date         string,
+    ptsw_book_from            string,
+    ptsw_book_to              string,
+    ptsw_book_subject         string,
+    ptsw_book_mission         string,
+    ptsw_book_note            string,
+    ptsw_book_fame            string,
+    ptsw_book_file            string, 
+    ) error {
   exists, err := s.AssetExists(ctx, id)
   if err != nil {
     return err
@@ -114,14 +252,20 @@ func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, i
 
   // overwriting original asset with new asset
   asset := Asset{
-    ID:             id,
-    Color:          color,
-    Size:           size,
-    Owner:          owner,
-    AppraisedValue: appraisedValue,
-    From: from,
-    Too: too,
-    BookType: bookType,
+    ID:                       id,
+    Ptsw_book_type:           ptsw_book_type,
+    Ptsw_book_receive_number: ptsw_book_receive_number,
+    Ptsw_book_receive_date:   ptsw_book_receive_date,
+    Ptsw_book_secret_class:   ptsw_book_secret_class,
+    Ptsw_book_number:         ptsw_book_number,
+    Ptsw_book_go_date:        ptsw_book_go_date,
+    Ptsw_book_from:           ptsw_book_from,
+    Ptsw_book_to:             ptsw_book_to,
+    Ptsw_book_subject:        ptsw_book_subject,
+    Ptsw_book_mission:        ptsw_book_mission,
+    Ptsw_book_note:           ptsw_book_note,
+    Ptsw_book_fame:           ptsw_book_fame,
+    Ptsw_book_file:           ptsw_book_file,
   }
   assetJSON, err := json.Marshal(asset)
   if err != nil {
@@ -155,6 +299,7 @@ func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface,
 }
 
 // TransferAsset updates the owner field of asset with given id in world state.
+/*
 func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterface, id string, newOwner string) error {
   asset, err := s.ReadAsset(ctx, id)
   if err != nil {
@@ -169,7 +314,7 @@ func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterfac
 
   return ctx.GetStub().PutState(id, assetJSON)
 }
-
+*/
 // GetAllAssets returns all assets found in world state
 func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*Asset, error) {
   // range query with empty string for startKey and endKey does an
